ca73856330195833436ab1392bd678b73621285e
Stoner5 <Stoner5@github.com>
2020-01-20 11:57:52 +0600

    Initial commit

---
+++ app/src/androidTest/java/com/e/basicsample/ActivityInputOutputTest.java
@@ -0,0 +1,57 @@
+package com.e.basicsample;
+
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(AndroidJUnit4.class)
+public class ActivityInputOutputTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.android.twoactivities",
+                appContext.getPackageName());
+    }
+
+    @Test
+    public void activityLaunch() {
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_second)).perform(click());
+        onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void textInputOutput() {
+        onView(withId(R.id.editText_main)).perform(
+                typeText("This is a test."));
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_message)).check(
+                matches(withText("This is a test.")));
+    }
+}
